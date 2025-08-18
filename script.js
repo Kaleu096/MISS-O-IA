e476a444498248b12201c45243bce96af3096b02
Kaleu096 <almeida.kaleu@escola.pr.gov.br>
2025-08-18 08:15:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,27 @@ const caixaPrincipal = document.querySelector(".caixa-pricipal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultados = document.querySelector(".caixa-resultados")
-const textoResultados = document.querySelector(".texto-resultdos")
+const textoPricipal = document.querySelector(".texto-pricipal")
+
+const perguntas = [
+    {
+     enunciado:"pergunta 1",
+    alternativas:["alternativa 1", "alternativa 2"]
+    }
+    {
+        enunciado:"pergunta 1",
+       alternativas:["alternativa 1", "alternativa 2"]
+       }
+       {
+        enunciado:"pergunta 1",
+       alternativas:["alternativa 1", "alternativa 2"]
+       }
+       {
+        enunciado:"pergunta 1",
+       alternativas:["alternativa 1", "alternativa 2"]
+       }
+       {
+        enunciado:"pergunta 1",
+       alternativas:["alternativa 1", "alternativa 2"]
+       }
+    ]
